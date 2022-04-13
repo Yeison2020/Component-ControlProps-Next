b6bc482469e3ee6f455b2a11fee14d739321f314
Yeison <enmanuelquiterios@gmail.com>
2022-04-13 11:09:28 -0400

    Updates

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -29,6 +29,7 @@ export const ShoppingPage = () => {
   const [shoppingCart, setshoppingCart] = useState<{
     [key: string]: ProductInCart;
   }>({});
+  console.log(shoppingCart);
 
   const onProductCountChange = ({
     count,
@@ -95,7 +96,10 @@ export const ShoppingPage = () => {
             style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
           />
           {/* <ProductTitle className="text-bold" /> */}
-          <ProductButtons className="custom-buttons" />
+          <ProductButtons
+            className="custom-buttons"
+            style={{ display: "flex", justifyContent: "center" }}
+          />
         </ProductCard>
       </div>
     </div>
